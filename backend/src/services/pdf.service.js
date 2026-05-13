f40b47083b5e2843743baf8b75ff90d18f2f684c
maguiar <maguiar@dnpi.gub.uy>
2026-05-13 00:49:48 -0300

    fix: require puppeteer-core instead of puppeteer in pdf.service.js

--- backend/src/services/pdf.service.js
+++ backend/src/services/pdf.service.js
@@ -1,4 +1,4 @@
-﻿const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const { PDFDocument } = require('pdf-lib');
 const forge = require('node-forge');
 const { generarQrDataUrl } = require('../utils/qr');
